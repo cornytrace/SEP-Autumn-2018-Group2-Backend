dde7ab5da8ed37783b1cfb1ee84760088e270ea6
Marten Kenbeek <marten.knbk@gmail.com>
2018-10-26 16:14:26 +0200

    Added additional logging.

--- eit_dashboard/settings.py
+++ eit_dashboard/settings.py
@@ -143,3 +143,15 @@ REST_FRAMEWORK = {
 
 # EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+        }
+    },
+}
